8c2b3a0ddc7061066c8631d20e8a930037b7ffbd
Oscar Rangel <r4ngel93@debian-DELL>
2020-01-13 17:38:41 -0600

    Updates

---
+++ precache-manifest.4465007480a65d87f4dacfcb7a4784a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fdcd7b7b3271435b3d91789e460409",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d5833c484876fcf26c93",
+    "url": "/pomodoro-clock/static/css/main.3c573c4c.chunk.css"
+  },
+  {
+    "revision": "33d499f47e2eda81d762",
+    "url": "/pomodoro-clock/static/js/2.59040c9b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.59040c9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5833c484876fcf26c93",
+    "url": "/pomodoro-clock/static/js/main.994ee13b.chunk.js"
+  },
+  {
+    "revision": "60646d41ad371ab84dd5",
+    "url": "/pomodoro-clock/static/js/runtime-main.793573d2.js"
+  }
+]);
